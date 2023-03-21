6c61972983fbbf5bf71278c6ab217a29121943af
knyL <linhleduc.it@hotmail.com>
2023-03-21 14:09:09 +0700

    feat: update metadata

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const SITE_TITLE = 'knyL';
-export const SITE_DESCRIPTION = 'I work creating software that solve problems in a simple way, and this place use to share what I learn along the way. The blog will focus on .NET Core and other related technologies. I hope you will gain some knowledge here on this blog. See y\'all around!';
+export const SITE_DESCRIPTION = 'I work creating software that solve problems in a simple way, and this place use to share what I learn along the way. The blog will focus on .NET Core and other related technologies. I hope you will gain some knowledge here on knyl.me. See y\'all around!';
